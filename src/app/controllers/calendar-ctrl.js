5d77d9591a9b7856878233069729fb4d066bc4a8
Valentin Pirgach <valentyn.pirhach@customertimes.com>
2016-08-10 17:08:56 +0300

    calendar controller tests

--- src/app/controllers/calendar-ctrl.js
+++ src/app/controllers/calendar-ctrl.js
@@ -1,15 +1,12 @@
 export default class CalendarCtrl {
-  constructor($scope, CalendarService) {
+  constructor(CalendarService) {
     this.dates = [];
     CalendarService.setDates(this.days);
-
     this.days = CalendarService.getDayNames();
     let dates = CalendarService.generateDates();
     this.dates = CalendarService.loadMoreDates(dates);
     this.selectedDates = [];
     this.CalendarService = CalendarService;
-
-    
   }
 
   getDate (_moment_) {
@@ -20,11 +17,6 @@ export default class CalendarCtrl {
     return _moment_.format('MMMM');
   }
 
-  renderSelected (period) {
-    let dates = period.sort((dateS, dateE) => dateS.date.isAfter(dateE.date));
-    this.CalendarService.renderSelected({dateStart: dates[0].date, dateEnd: dates[1].date});
-  }
-
   specificPriceClasses (date) {
     return {
       first: date.specificPrice._first,
@@ -33,43 +25,25 @@ export default class CalendarCtrl {
     };
   }
 
-  availCell (cell) {
+  cellStyle (cell) {
 		//same day
 		if(cell._first && cell._last) {
-			let width = moment(cell._last).diff(cell._first, 'm') * 100 / 1440;
-			let left = moment(cell._first).diff(cell._first.clone().startOf('day'), 'm') * 100 / 1440;
-
-			return {width: `${width}%`, left: `${left}%`};
-		}
-
-		if(cell._first)
-			return {width: `${100 - moment(cell._first).diff(cell._first.clone().startOf('day'), 'm') * 100 / 1440}%`, right: 0};
-
-		if(cell._last)
-			return {width: `${moment(cell._last).diff(cell._last.clone().startOf('day'), 'm') * 100 / 1440}%`, left: 0};
-
-		return {width: `100%`};
-	}
-
-  selectedCell (cell) {
-		//same day
-		if(cell._first && cell._last) {
-			let width = moment(cell._last).diff(cell._first, 'm') * 100 / 1440;
-			let left = moment(cell._first).diff(cell._first.clone().startOf('day'), 'm') * 100 / 1440;
-			return {width: `${width}%`, left: `${left}%`};
+			let width = cell._last.diff(cell._first, 'm') * 100 / 1440;
+			let left = cell._first.diff(cell._first.clone().startOf('day'), 'm') * 100 / 1440;
+			return {width: `${parseFloat(width.toFixed(4))}%`, left: `${parseFloat(left).toFixed(4)}%`};
 		}
 
 		if(cell._first) {
 			return {
-        width: `${100 - moment(cell._first).diff(cell._first.clone().startOf('day'), 'm') * 100 / 1440}%`,
+        width: `${parseFloat((100 - cell._first.diff(cell._first.clone().startOf('day'), 'm') * 100 / 1440).toFixed(4))}%`,
         right: '0'
       };
     }
 
 		if(cell._last) {
 			return {
-        width: `${moment(cell._last).diff(cell._last.clone().startOf('day'), 'm') * 100 / 1440}%`,
-        left: 0
+        width: `${parseFloat((cell._last.diff(cell._last.clone().startOf('day'), 'm') * 100 / 1440).toFixed(4))}%`,
+        left: '0'
       };
     }
 
